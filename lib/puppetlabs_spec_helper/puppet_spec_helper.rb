15b5e003c569d95dd87ce62fbe008bc64506bd91
Branan Purvine-Riley <branan@puppetlabs.com>
2012-07-05 15:21:00 -0700

    Work around an annoyance of mocha 0.12.0

--- lib/puppetlabs_spec_helper/puppet_spec_helper.rb
+++ lib/puppetlabs_spec_helper/puppet_spec_helper.rb
@@ -3,6 +3,9 @@ require 'puppetlabs_spec_helper/puppetlabs_spec_helper'
 # Don't want puppet getting the command line arguments for rake or autotest
 ARGV.clear
 
+# This is needed because we're using mocha with rspec instead of Test::Unit or MiniTest
+ENV['MOCHA_OPTIONS']='skip_integration'
+
 require 'puppet'
 require 'mocha'
 gem 'rspec', '>=2.0.0'
