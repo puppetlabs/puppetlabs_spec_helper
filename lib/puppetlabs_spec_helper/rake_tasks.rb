3338d0315713f7e7ae2600372ac119f5f9ea0034
Tim Sharpe <tim@sharpe.id.au>
2017-08-10 16:09:04 +1000

    (PDK-381) Ensure spec fixtures are cleaned up, even if the test fails

--- lib/puppetlabs_spec_helper/rake_tasks.rb
+++ lib/puppetlabs_spec_helper/rake_tasks.rb
@@ -368,9 +368,12 @@ end
 
 desc "Run spec tests and clean the fixtures directory if successful"
 task :spec do
-  Rake::Task[:spec_prep].invoke
-  Rake::Task[:spec_standalone].invoke
-  Rake::Task[:spec_clean].invoke
+  begin
+    Rake::Task[:spec_prep].invoke
+    Rake::Task[:spec_standalone].invoke
+  ensure
+    Rake::Task[:spec_clean].invoke
+  end
 end
 
 desc "Parallel spec tests"
@@ -384,9 +387,10 @@ task :parallel_spec do
 
     Rake::Task[:spec_prep].invoke
     ParallelTests::CLI.new.run(args)
-    Rake::Task[:spec_clean].invoke
   rescue LoadError
     raise 'Add the parallel_tests gem to Gemfile to enable this task'
+  ensure
+    Rake::Task[:spec_clean].invoke
   end
 end
 
