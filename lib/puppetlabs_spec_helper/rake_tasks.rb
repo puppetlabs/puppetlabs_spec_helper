9ac3b071ecf2bf1c3e7bffe630232a8ceecee46b
Erick Banks <erick@puppetlabs.com>
2018-02-16 12:35:03 -0800

    (FM-6813) fix parsing for test tiers
    Original code did not properly tag tiers.

--- lib/puppetlabs_spec_helper/rake_tasks.rb
+++ lib/puppetlabs_spec_helper/rake_tasks.rb
@@ -57,22 +57,27 @@ end
 
 desc "Run beaker acceptance tests"
 RSpec::Core::RakeTask.new(:beaker) do |t|
-  t.rspec_opts = ['--color']
-  t.pattern = 'spec/acceptance'
-  # TEST_TIERS env variable is a comma separated list of tiers to run. e.g. low, medium, high
-  if ENV['TEST_TIERS']
-    tiers = '--tag '
-    test_tiers = ENV['TEST_TIERS'].split(',')
-    raise 'TEST_TIERS env variable must have at least 1 tier specified. low, medium or high (comma separated).' if test_tiers.count == 0
-    test_tiers.each do |tier|
-      tier_to_add = tier.strip
-      raise "#{tier_to_add} not a valid test tier." unless %w(low medium high).include?(tier_to_add)
-      tiers += "tier_#{tier_to_add},"
+  SetupBeaker.setup_beaker(t)
+end
+
+class SetupBeaker
+  def self.setup_beaker(t)
+    t.rspec_opts = ['--color']
+    t.pattern = 'spec/acceptance'
+    # TEST_TIERS env variable is a comma separated list of tiers to run. e.g. low, medium, high
+    if ENV['TEST_TIERS']
+      test_tiers = ENV['TEST_TIERS'].split(',')
+      raise 'TEST_TIERS env variable must have at least 1 tier specified. low, medium or high (comma separated).' if test_tiers.count == 0
+      test_tiers.each do |tier|
+        tier_to_add = tier.strip.downcase
+        raise "#{tier_to_add} not a valid test tier." unless %w(low medium high).include?(tier_to_add)
+        tiers = "--tag tier_#{tier_to_add}"
+        t.rspec_opts.push(tiers)
+      end
+    else
+      puts 'TEST_TIERS env variable not defined. Defaulting to run all tests.'
     end
-    tiers = tiers.chomp(',')
-    t.rspec_opts.push(tiers)
-  else
-    puts 'TEST_TIERS env variable not defined. Defaulting to run all tests.'
+    return t
   end
 end
 
--- spec/unit/puppetlabs_spec_helper/rake_task_spec.rb
+++ spec/unit/puppetlabs_spec_helper/rake_task_spec.rb
@@ -1,8 +1,35 @@
 require 'puppetlabs_spec_helper/puppet_spec_helper'
 require 'puppetlabs_spec_helper/rake_tasks'
+
 RSpec.configure do |config|
   config.mock_with :rspec
   config.include PuppetlabsSpecHelper::RakeTasks
+end
+
+describe SetupBeaker do
+  describe '.setup_beaker' do
+    let(:task) {RSpec::Core::RakeTask.new}
+    it 'can set tag for low tier' do
+      allow(ENV).to receive(:[]).and_return('low')
+      expect(SetupBeaker.setup_beaker(task).rspec_opts.to_s).to match(/--tag tier_low/)
+    end
+    it 'can set tag for high, medium and low tier' do
+      allow(ENV).to receive(:[]).and_return('high, medium, low')
+      expect(SetupBeaker.setup_beaker(task).rspec_opts.to_s).to match(/--tag tier_high/ && /--tag tier_medium/ && /--tag tier_low/)
+    end
+    it 'does not set a tag when ENV[TEST_TIERS] is nil' do
+      allow(ENV).to receive(:[]).and_return(nil)
+      expect(SetupBeaker.setup_beaker(task).rspec_opts.to_s).to_not match(/--tag/)
+    end
+    it 'errors when tier specified does not exist' do
+      allow(ENV).to receive(:[]).and_return('expect_error')
+      expect{SetupBeaker.setup_beaker(task)}.to raise_error(RuntimeError, /not a valid test tier/)
+    end
+    it 'errors when tiers are quoted' do
+      allow(ENV).to receive(:[]).and_return('"high", "medium", "low"')
+      expect{SetupBeaker.setup_beaker(task)}.to raise_error(RuntimeError, /not a valid test tier/)
+    end
+  end
 end
 
 describe PuppetlabsSpecHelper::RakeTasks do
