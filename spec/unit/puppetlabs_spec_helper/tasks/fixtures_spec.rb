fa27f487b244a3354d6cf6ebac032616a7842ea2
David Schmitt <david.schmitt@puppet.com>
2018-06-16 14:44:26 +0100

    Realign the tests with rubocop preferring `exist?` over `exists?`

--- spec/unit/puppetlabs_spec_helper/tasks/fixtures_spec.rb
+++ spec/unit/puppetlabs_spec_helper/tasks/fixtures_spec.rb
@@ -8,12 +8,13 @@ describe PuppetlabsSpecHelper::Tasks::FixtureHelpers do
       PuppetlabsSpec::Fixtures.instance_methods.each do |m|
         PuppetlabsSpec::Fixtures.send(:undef_method, m)
       end
-      allow(File).to receive(:exists?).with('.fixtures.yml').and_return false
-      allow(File).to receive(:exists?).with('.fixtures.yaml').and_return false
+      allow(File).to receive(:exist?).with('.fixtures.yml').and_return false
+      allow(File).to receive(:exist?).with('.fixtures.yaml').and_return false
       allow(ENV).to receive(:[]).and_call_original
       allow(ENV).to receive(:[]).with('FIXTURES_YML').and_return(nil)
       allow(described_class).to receive(:auto_symlink).and_return('project' => '#{source_dir}')
     end
+
     context 'when file is missing' do
       it 'returns basic directories per category' do
         expect(subject.fixtures('forge_modules')).to eq({})
@@ -22,7 +23,7 @@ describe PuppetlabsSpecHelper::Tasks::FixtureHelpers do
     end
     context 'when file is empty' do
       it 'returns basic directories per category' do
-        allow(File).to receive(:exists?).with('.fixtures.yml').and_return true
+        allow(File).to receive(:exist?).with('.fixtures.yml').and_return true
         allow(YAML).to receive(:load_file).with('.fixtures.yml').and_return false
         expect(subject.fixtures('forge_modules')).to eq({})
         expect(subject.fixtures('repositories')).to eq({})
@@ -30,21 +31,21 @@ describe PuppetlabsSpecHelper::Tasks::FixtureHelpers do
     end
     context 'when file is malformed' do
       it 'raises an error' do
-        allow(File).to receive(:exists?).with('.fixtures.yml').and_return true
-        allow(YAML).to receive(:load_file).with('.fixtures.yml').and_raise(Psych::SyntaxError.new('/file', '123', '0', '0', 'spec message', 'spec context'))
+        expect(File).to receive(:exist?).with('.fixtures.yml').and_return true
+        expect(YAML).to receive(:load_file).with('.fixtures.yml').and_raise(Psych::SyntaxError.new('/file', '123', '0', '0', 'spec message', 'spec context'))
         expect { subject.fixtures('forge_modules') }.to raise_error(RuntimeError, %r{malformed YAML})
       end
     end
     context 'when file contains no fixtures' do
       it 'raises an error' do
-        allow(File).to receive(:exists?).with('.fixtures.yml').and_return true
+        allow(File).to receive(:exist?).with('.fixtures.yml').and_return true
         allow(YAML).to receive(:load_file).with('.fixtures.yml').and_return('some' => 'key')
         expect { subject.fixtures('forge_modules') }.to raise_error(RuntimeError, %r{No 'fixtures'})
       end
     end
     context 'when file specifies fixtures' do
       it 'returns the hash' do
-        allow(File).to receive(:exists?).with('.fixtures.yml').and_return true
+        allow(File).to receive(:exist?).with('.fixtures.yml').and_return true
         allow(YAML).to receive(:load_file).with('.fixtures.yml').and_return('fixtures' => { 'forge_modules' => { 'stdlib' => 'puppetlabs-stdlib' } })
         expect(subject.fixtures('forge_modules')).to eq('puppetlabs-stdlib' => {
                                                           'target' => 'spec/fixtures/modules/stdlib',
@@ -58,7 +59,7 @@ describe PuppetlabsSpecHelper::Tasks::FixtureHelpers do
     end
     context 'when file specifies defaults' do
       it 'returns the hash' do
-        allow(File).to receive(:exists?).with('.fixtures.yml').and_return true
+        allow(File).to receive(:exist?).with('.fixtures.yml').and_return true
         allow(YAML).to receive(:load_file).with('.fixtures.yml').and_return('defaults' => { 'forge_modules' => { 'flags' => '--module_repository=https://myforge.example.com/' } },
                                                                             'fixtures' => { 'forge_modules' => { 'stdlib' => 'puppetlabs-stdlib' } })
         expect(subject.fixtures('forge_modules')).to eq('puppetlabs-stdlib' => {
