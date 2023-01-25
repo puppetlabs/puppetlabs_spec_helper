ca4e730f2d9f327dfe08ac3d346d54b1e8816677
Craig Gumbley <craiggumbley@gmail.com>
2023-01-25 10:31:49 +0000

    (MAINT) Update module and class style

--- lib/puppetlabs_spec_helper/puppetlabs_spec/fixtures.rb
+++ lib/puppetlabs_spec_helper/puppetlabs_spec/fixtures.rb
@@ -1,53 +1,55 @@
 # frozen_string_literal: true
 
-# This module provides some helper methods to assist with fixtures. It's
-# methods are designed to help when you have a conforming fixture layout so we
-# get project consistency.
-module PuppetlabsSpec::Fixtures
-  # Returns the joined path of the global FIXTURE_DIR plus any path given to it
-  def fixtures(*rest)
-    File.join(PuppetlabsSpec::FIXTURE_DIR, *rest)
-  end
-
-  # Returns the path to your relative fixture dir. So if your spec test is
-  # <project>/spec/unit/facter/foo_spec.rb then your relative dir will be
-  # <project>/spec/fixture/unit/facter/foo
-  def my_fixture_dir
-    callers = caller
-    while (line = callers.shift)
-      next unless (found = line.match(%r{/spec/(.*)_spec\.rb:}))
-
-      return fixtures(found[1])
-    end
-    raise "sorry, I couldn't work out your path from the caller stack!"
-  end
-
-  # Given a name, returns the full path of a file from your relative fixture
-  # dir as returned by my_fixture_dir.
-  def my_fixture(name)
-    file = File.join(my_fixture_dir, name)
-    unless File.readable? file
-      raise "fixture '#{name}' for #{my_fixture_dir} is not readable"
+module PuppetlabsSpec
+  # This module provides some helper methods to assist with fixtures. It's
+  # methods are designed to help when you have a conforming fixture layout so we
+  # get project consistency.
+  module Fixtures
+    # Returns the joined path of the global FIXTURE_DIR plus any path given to it
+    def fixtures(*rest)
+      File.join(PuppetlabsSpec::FIXTURE_DIR, *rest)
     end
 
-    file
-  end
+    # Returns the path to your relative fixture dir. So if your spec test is
+    # <project>/spec/unit/facter/foo_spec.rb then your relative dir will be
+    # <project>/spec/fixture/unit/facter/foo
+    def my_fixture_dir
+      callers = caller
+      while (line = callers.shift)
+        next unless (found = line.match(%r{/spec/(.*)_spec\.rb:}))
 
-  # Return the contents of the file using read when given a name. Uses
-  # my_fixture to work out the relative path.
-  def my_fixture_read(name)
-    File.read(my_fixture(name))
-  end
-
-  # Provides a block mechanism for iterating across the files in your fixture
-  # area.
-  def my_fixtures(glob = '*', flags = 0, &block)
-    files = Dir.glob(File.join(my_fixture_dir, glob), flags)
-    if files.empty?
-      raise "fixture '#{glob}' for #{my_fixture_dir} had no files!"
+        return fixtures(found[1])
+      end
+      raise "sorry, I couldn't work out your path from the caller stack!"
     end
 
-    block && files.each(&block)
-    files
+    # Given a name, returns the full path of a file from your relative fixture
+    # dir as returned by my_fixture_dir.
+    def my_fixture(name)
+      file = File.join(my_fixture_dir, name)
+      unless File.readable? file
+        raise "fixture '#{name}' for #{my_fixture_dir} is not readable"
+      end
+
+      file
+    end
+
+    # Return the contents of the file using read when given a name. Uses
+    # my_fixture to work out the relative path.
+    def my_fixture_read(name)
+      File.read(my_fixture(name))
+    end
+
+    # Provides a block mechanism for iterating across the files in your fixture
+    # area.
+    def my_fixtures(glob = '*', flags = 0, &block)
+      files = Dir.glob(File.join(my_fixture_dir, glob), flags)
+      if files.empty?
+        raise "fixture '#{glob}' for #{my_fixture_dir} had no files!"
+      end
+
+      block && files.each(&block)
+      files
+    end
   end
 end
--- lib/puppetlabs_spec_helper/puppetlabs_spec/puppet_internals.rb
+++ lib/puppetlabs_spec_helper/puppetlabs_spec/puppet_internals.rb
@@ -4,41 +4,43 @@
 # 'puppetlabs_spec_helper/puppet_spec_helper' library
 require 'puppetlabs_spec_helper/puppet_spec_helper'
 
-# PuppetInternals provides a set of methods that interface
-# with internal puppet implementations.
-module PuppetlabsSpec::PuppetInternals
-  def resource(parts = {})
-    resource_type = parts[:type] || :hostclass
-    resource_name = parts[:name] || 'testing'
-    Puppet::Resource::Type.new(resource_type, resource_name)
-  end
-  module_function :resource
+module PuppetlabsSpec
+  # PuppetInternals provides a set of methods that interface
+  # with internal puppet implementations.
+  module PuppetInternals
+    def resource(parts = {})
+      resource_type = parts[:type] || :hostclass
+      resource_name = parts[:name] || 'testing'
+      Puppet::Resource::Type.new(resource_type, resource_name)
+    end
+    module_function :resource
 
-  def compiler(parts = {})
-    compiler_node = parts[:node] || node
-    Puppet::Parser::Compiler.new(compiler_node)
-  end
-  module_function :compiler
+    def compiler(parts = {})
+      compiler_node = parts[:node] || node
+      Puppet::Parser::Compiler.new(compiler_node)
+    end
+    module_function :compiler
 
-  def node(parts = {})
-    node_name = parts[:name] || 'testinghost'
-    options = parts[:options] || {}
-    node_environment = Puppet::Node::Environment.create(parts[:environment] || 'test', [])
-    options[:environment] = node_environment
-    Puppet::Node.new(node_name, options)
-  end
-  module_function :node
+    def node(parts = {})
+      node_name = parts[:name] || 'testinghost'
+      options = parts[:options] || {}
+      node_environment = Puppet::Node::Environment.create(parts[:environment] || 'test', [])
+      options[:environment] = node_environment
+      Puppet::Node.new(node_name, options)
+    end
+    module_function :node
 
-  # Return a method instance for a given function.  This is primarily useful
-  # for rspec-puppet
-  def function_method(name, parts = {})
-    scope = parts[:scope] || scope()
-    # Ensure the method instance is defined by side-effect of checking if it
-    # exists.  This is a hack, but at least it's a hidden hack and not an
-    # exposed hack.
-    return nil unless Puppet::Parser::Functions.function(name)
+    # Return a method instance for a given function.  This is primarily useful
+    # for rspec-puppet
+    def function_method(name, parts = {})
+      scope = parts[:scope] || scope()
+      # Ensure the method instance is defined by side-effect of checking if it
+      # exists.  This is a hack, but at least it's a hidden hack and not an
+      # exposed hack.
+      return nil unless Puppet::Parser::Functions.function(name)
 
-    scope.method("function_#{name}".to_sym)
+      scope.method("function_#{name}".to_sym)
+    end
+    module_function :function_method
   end
-  module_function :function_method
 end
